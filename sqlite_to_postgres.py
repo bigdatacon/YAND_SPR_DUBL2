ed071c3f68abbcb4a7ed1b52a3e93ff34b5d0d33
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-20 14:29:21 +0300

    Запустил постгре на 5433

--- sqlite_to_postgres.py
+++ sqlite_to_postgres.py
@@ -212,6 +212,7 @@ if __name__ == '__main__':
     # dsl = {'dbname': 'movies', 'user': 'postgres', 'password': 'postgres', 'host': '127.0.0.1', 'port': 5433}
     # dsl = {'dbname': 'postgres', 'user': 'postgres', 'password': 'postgres', 'host': '127.0.0.1', 'port': 5433}
     # dsl = {'dbname': 'postgres', 'user': 'postgres', 'password': 'postgres', 'host': 'postgres', 'port': 5433}
-    dsl = {'dbname': 'movies', 'user': 'postgres', 'password': 'postgres', 'host': 'postgres', 'port': 5432}
+    # dsl = {'dbname': 'movies', 'user': 'postgres', 'password': 'postgres', 'host': 'postgres', 'port': 5432}
+    dsl = {'dbname': 'postgres', 'user': 'postgres', 'password': 'postgres', 'host': 'postgres', 'port': 5433}
     with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)
