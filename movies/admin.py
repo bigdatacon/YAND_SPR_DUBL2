7bfdc87b13dea88f3e14c1f9d65e05d9b0b074c9
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-21 02:11:21 +0300

    Запустил постгре на 5433

--- movies/admin.py
+++ movies/admin.py
@@ -1,52 +1,6 @@
 from django.contrib import admin
-from .models import FilmWorkMovie, PersonFilmWork, Genre, Person, GenreFilmWork
+from .models import FilmWorkMovie
 
-
-class PersonFilmWorkInline(admin.TabularInline):
-    model = PersonFilmWork
-    extra = 1
-
-
-class GenreFilmWorkInline(admin.TabularInline):
-    model = GenreFilmWork
-    extra = 1
-
-
-@admin.register(FilmWorkMovie)
-class FilmworkAdmin(admin.ModelAdmin):
-    # отображение полей в списке
-    list_display = ('title', 'type', 'creation_date', 'rating',)
-    # порядок следования полей в форме создания/редактирования
-
-    # фильтрация в списке
-    list_filter = ('type', 'rating')
-
-    # поиск по полям
-    search_fields = ('title', 'type')
-
-    fields = (
-        'title',
-        'type',
-        'description',
-        'creation_date',
-        #'certificate',
-         'rating',
-    )
-
-    inlines = [
-        PersonFilmWorkInline, GenreFilmWorkInline
-    ]
-
-
-@admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Person)
-class PersonAdmin(admin.ModelAdmin):
-    search_fields = ('full_name',)
-    pass
 
 
 @admin.register(FilmWorkMovie)
