319f93d4ff31ee1127784bff7705382f21ec9e3f
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-21 02:06:07 +0300

    Запустил постгре на 5433

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from .models import FilmWorkMovie, PersonFilmWork, Genre, Person, GenreFilmWork
 
-# Register your models here.
+
+class PersonFilmWorkInline(admin.TabularInline):
+    model = PersonFilmWork
+    extra = 1
+
+
+class GenreFilmWorkInline(admin.TabularInline):
+    model = GenreFilmWork
+    extra = 1
+
+
+@admin.register(FilmWorkMovie)
+class FilmworkAdmin(admin.ModelAdmin):
+    # отображение полей в списке
+    list_display = ('title', 'type', 'creation_date', 'rating',)
+    # порядок следования полей в форме создания/редактирования
+
+    # фильтрация в списке
+    list_filter = ('type', 'rating')
+
+    # поиск по полям
+    search_fields = ('title', 'type')
+
+    fields = (
+        'title',
+        'type',
+        'description',
+        'creation_date',
+        #'certificate',
+         'rating',
+    )
+
+    inlines = [
+        PersonFilmWorkInline, GenreFilmWorkInline
+    ]
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ('full_name',)
+    pass
+
+
+@admin.register(FilmWorkMovie)
+class FilmworkAdmin(admin.ModelAdmin):
+    # отображение полей в списке
+    list_display = ('title', 'type', 'creation_date', 'rating',)
+    # порядок следования полей в форме создания/редактирования
+    fields = (
+        'title', 'type', 'description', 'creation_date',
+        'rating',
+    )
