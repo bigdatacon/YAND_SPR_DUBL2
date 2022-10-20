b5a689c9b859bb6d18628e995e45e1f53d514d9d
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-20 19:41:47 +0300

    Запустил постгре на 5433

--- sqlite_to_postgres.py
+++ sqlite_to_postgres.py
@@ -71,17 +71,20 @@ class PostgresSaver:
         genre_old_and_new_ids = []
         person_old_and_new_ids = []
         # for obj in data['FilmWork']:
+        print(f' len() data["film_workmovie"] : {len(data["film_workmovie"])}, len(data["person"]) : {len(data["person"])}')
+        for obj in data:
+            print(f'here obj : {obj}')
         for obj in data['film_workmovie']:
             new_id = uuid.uuid4()
             film_work_old_and_new_ids.append({new_id: str(obj.id)})
             to_insert = (
                 new_id, obj.title,  obj.description,
-                datetime.datetime.now(), None, "movie", datetime.datetime.now(), datetime.datetime.now(), obj.rating
+                datetime.datetime.now(),  "movie", datetime.datetime.now(), datetime.datetime.now(), obj.rating
             )
             self.pg_cursor.execute(
                 """INSERT INTO content.film_workmovie (id, title, description, creation_date, 
                 type, created_at, updated_at, rating  )
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""", to_insert
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""", to_insert
             )
         for obj_actors in data['genre']:
             new_id = uuid.uuid4()
@@ -90,6 +93,7 @@ class PostgresSaver:
             to_insert = (new_id, obj_actors.name, obj_actors.description, obj_actors.created_at, obj_actors.updated_at)
             self.pg_cursor.execute("""INSERT INTO content.genre (id, name, description, created_at, updated_at)
                 VALUES (%s, %s, %s, %s, %s)""", to_insert)
+            print(f' did INSERT in genre')
         for genre_film_work in data['genre_film_work']:
             film_work_id = None
             genre_id = None
@@ -125,19 +129,23 @@ class PostgresSaver:
             for film_work_old_and_new_id in film_work_old_and_new_ids:
                 if str(person_film_work.film_work_id) in film_work_old_and_new_id.values():
                     film_work_id = list(film_work_old_and_new_id.keys())[0]
-                    print(f'here film_work_id : {film_work_id}')
+                    # print(f'here film_work_id : {film_work_id}')
                     break
 
             for person_old_and_new_id in person_old_and_new_ids:
                 if str(person_film_work.person_id) in person_old_and_new_id.values():
                     person_id = list(person_old_and_new_id.keys())[0]
-                    print(f'here genr.id : {genre_id }')
+                    # print(f'here genr.id : {genre_id }')
                     break
             new_id = uuid.uuid4()
             to_insert = (new_id, film_work_id, person_id, person_film_work.role, person_film_work.created_at)
+            # print(f' eto to_insert in person_film_work : {to_insert}')
             if film_work_id and person_id:
-                self.pg_cursor.execute("""INSERT INTO content.person_film_work (id, film_work_id, person_id, role, created_at)
-                    VALUES (%s, %s, %s, %s, %s)""", to_insert)
+                try:
+                    self.pg_cursor.execute("""INSERT INTO content.person_film_work (id, film_work_id, person_id, role, created_at)
+                        VALUES (%s, %s, %s, %s, %s)""", to_insert)
+                except Exception as e:
+                    print(f'except in  INSERT INTO content.person_film_work : {e.args}')
 
 class SQLiteLoader:
     def __init__(self, connection: sqlite3.Connection):
@@ -165,12 +173,14 @@ class SQLiteLoader:
             print(f'eto len(temp) : {len(temp)}')
             i = 0
             for obj in temp:
-                if table_name[0] == "film_workmovie":
+                # if table_name[0] == "film_workmovie":
+                if table_name[0] == "film_work":
                     if temp[i][5] != "N/A":
                         rating = temp[i][5]
                     else:
                         rating = 0
-                    data[table_name[0]].append(
+                    # data[table_name[0]].append(
+                    data['film_workmovie'].append(
                         FilmWorkMovie(
                             id = temp[i][0],
                             title = temp[i][1],
