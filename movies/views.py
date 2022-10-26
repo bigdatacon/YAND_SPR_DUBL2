9bdcbf30940ae62b2335020c1d42f23aafef61c9
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-26 21:29:03 +0300

    добавил view И прописал для него url

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from .models import FilmWorkMovie
+from rest_framework import viewsets
+from rest_framework import permissions
+
+
 
 # Create your views here.
+def index(request):
+    data = []
+    for film in FilmWorkMovie.objects.all():
+        film_info = {
+            'uuid': film.id,
+            'title': film.title,
+            'description': film.description,
+            'rating': film.rating,
+            # 'genres': film.genres,
+            # 'persons': film.persons,
+        }
+        # for furl in film.files.all():
+        #     film_info['files'].append(furl.file_path.url)
+        data.append(film_info)
+    return JsonResponse({'results': data})
