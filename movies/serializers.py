0ad49b8e65d085c3e98b03d7331b4d0e154308fd
root <root@ovz4.j54523970.pv29m.vps.myjino.ru>
2022-10-27 21:51:41 +0300

    Genre/person serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import routers, serializers, viewsets
-from .models import FilmWorkMovie
+from .models import FilmWorkMovie, Genre, Person
 
 # Serializers define the API representation.
 #тут Важный вопрос что если добавить жанры то все падает
 
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['id', 'title', 'description']
+
+
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ['id', 'full_name']
+
+
 class FilmWorkMovieSerializer(serializers.HyperlinkedModelSerializer):
+    genres = GenreSerializer(many=True)
+    persons = PersonSerializer(many=True)
+
     class Meta:
         model = FilmWorkMovie
-        fields = ['id', 'title', 'description', 'rating']
+        fields = ['id', 'title', 'description', 'rating', 'genres', 'persons']
