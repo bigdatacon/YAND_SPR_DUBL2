804d36d09ed4fa6678ded769dbd05153d9b80fa2
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-29 11:54:46 +0300

    перенесены правки из thre_1 внесены правки в sqlite_to_postgres с учетом изменения полей моделей

--- api_test.py
+++ api_test.py
@@ -6,4 +6,7 @@ genre= 'Action'
 res = requests.get(f"http://127.0.0.1:8080/filmworkmovie_view/?genre={genre}")
 # print(f' eto res : {res.json()}')
 for i in res.json():
-    print(i.get('title') , i.get('genres') )
+    genre_list = []
+    for j in i.get('genres'):
+        genre_list.append(j.get('name'))
+    print(i.get('title') , genre_list )
