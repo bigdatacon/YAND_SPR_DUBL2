214c098b58a213cef1bfd340d8bd4b1356776650
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-29 07:43:14 +0300

    перенесены правки из thre_1 внесены правки в sqlite_to_postgres с учетом изменения полей моделей

--- api_test.py
+++ api_test.py
@@ -1,6 +1,6 @@
 import requests
-# res = requests.get("http://127.0.0.1:8080/filmworkmovie_view/?min_rating=8")
-res = requests.get("http://127.0.0.1:8080/filmworkmovie_view/?actor='Carrie Fisher'")
+res = requests.get("http://127.0.0.1:8080/filmworkmovie_view/?min_rating=8")
+# res = requests.get("http://127.0.0.1:8080/filmworkmovie_view/?actor='Carrie Fisher'")
 # print(f' eto res : {res.json()}')
 for i in res.json():
     print(i.get('title') , i.get('rating') )
