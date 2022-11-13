c2f4f3239fb38866f1e669c79cd9da4f54dc1928
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-11-14 02:22:21 +0300

    эластик начало

--- pg_to_es_two/es_saver_two.py
+++ pg_to_es_two/es_saver_two.py
@@ -4,164 +4,148 @@ from elasticsearch import Elasticsearch, helpers
 from typing import List
 logger = logging.getLogger(__name__)
 
+#teory https://github.com/elastic/elasticsearch-py/blob/main/examples/bulk-ingest/bulk-ingest.py
+#teory good https://github.com/elastic/elasticsearch-py/issues/1698
+
 # from settings.settings import Settings
 # from settings.schemes import Schemes
 from settings_two import Settings
 from schemes import Schemes
 from resources_two import backoff
+from elasticsearch import  Elasticsearch
+
+
+
+#!/usr/bin/env python
+# Licensed to Elasticsearch B.V under one or more agreements.
+# Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+# See the LICENSE file in the project root for more information
+
+"""Script that downloads a public dataset and streams it to an Elasticsearch cluster"""
+
+import csv
+from os.path import abspath, join, dirname, exists
+import tqdm
+import urllib3
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import streaming_bulk
+
+
+NYC_RESTAURANTS = (
+    "https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv?accessType=DOWNLOAD"
+)
+DATASET_PATH = join(dirname(abspath(__file__)), "nyc-restaurants.csv")
+CHUNK_SIZE = 16384
+
 
 
 class ESSaver(Settings, Schemes):
 
-    #tutorial: https://www.youtube.com/watch?v=LchUgSMPDRI
-
-    __es_con = None
-
-    SCHEMES = {
-        "movies": 'film_scheme',
-        "persons": 'person_scheme',
-        "genres": 'genre_scheme',
-    }
-
-    test_schemes = {
-    "mappings": {
-        "properties": {
-            "text_field": {"type": "keyword"},
-            "number": {"type": "long"}
-        }
-    }
-    }
-
-    @backoff()
-    def save_one(self, doc: dict, index: str):
-        self.__get_connection().index(index=index, id=doc['id'], document=doc)
-
-    @backoff()
-    def save_many(self, docs: List[dict], index: str):
-        helpers.bulk(self.__get_connection(), [{'_index': index, '_id': doc['id'], **doc} for doc in docs])
-
-    def __get_connection(self):
-        if not self.__es_con:
-            self.__es_con = Elasticsearch(self.__get_es_link())
-        return self.__es_con
 
     def __get_es_link(self):
-        es_params = dict(self.get_settings().film_work_es)
-        return f"http://{es_params['host']}:{es_params['port']}"
+        return f'http://{self.get_settings().film_work_es.host}:{self.get_settings().film_work_es.port}'
 
-    @backoff()
-    def create_index(self, index: str):
-        scheme = self.get_schemes()[self.SCHEMES[index]]
-        # logger.debug(f"this scheme: {scheme}")
-        resp = requests.put("{}/{}".format(self.__get_es_link(), index), json=scheme)
+    def __get_es_client(self):
+        return Elasticsearch(self.__get_es_link())
+
+    def __delete_index(self, index_name):
+        resp = requests.delete("{}/{}".format(self.__get_es_link(), index_name))
         if resp.status_code != 200:
-            logger.warning(f"Ошибка создания поискового индекса: {index}")
-
-    """block of test functions for 127.0.0.1 поскольку с хост elastic не работает, нужно именно 127.0.0.1 , функции повторяют 
-    функционал основных функций"""
-    def test_save_one(self, doc: dict, index: str):
-        try:
-            self.test_get_connection().index(index=index,  document=doc)
-        except Exception as e:
-            print(f' except in test_save_one : {e.args}')
-
-    #quick read index
-    def test_read_index(self, index):
-        ans = self.test_get_connection().search(index=index)
-        return ans
-
-    def test_save_many(self, docs: List[dict], index: str):
-        helpers.bulk(self.test_get_connection(), [{'_index': index,  **doc} for doc in docs])
-
-    def test_get_connection(self):
-        host_link = self.__get_es_link().replace('elastic', '127.0.0.1')
-        if not self.__es_con:
-            self.__es_con = Elasticsearch(host_link)
-        return self.__es_con
-
-    def test_get_scheme(self, index: str):
-        scheme = self.get_schemes()[self.SCHEMES[index]]
-        return scheme
-
-
-    def test_delete_index(self, index: str):
-        host_link = self.__get_es_link().replace('elastic', '127.0.0.1')
-        resp = requests.delete("{}/{}".format(host_link, index))
-        if resp.status_code != 200:
-            logger.warning(f"Ошибка создания поискового индекса: {resp.status_code, index}")
+            logger.warning(f"Ошибка создания поискового индекса: {resp.status_code, index_name}")
         else:
-            logger.warning(f"DELETE индекс: {resp.status_code, index}")
+            logger.warning(f"DELETE индекс: {resp.status_code, index_name}")
 
-    # @backoff()
-    def test_create_index_127(self, index: str):
-        scheme = self.get_schemes()[self.SCHEMES[index]]
-        host_link = self.__get_es_link().replace('elastic', '127.0.0.1')
-        logger.warning(f"this scheme: {scheme, host_link}")
-        resp = requests.put("{}/{}".format(host_link, index), json=scheme)
-        if resp.status_code != 200:
-            logger.warning(f"Ошибка создания поискового индекса: {resp.status_code, index}")
+    def create_index2(self, index_name):
 
-        else:
-            logger.warning(f"Create индекс: {resp.status_code, index}")
-
-    def test_create_index_on_test_scheme(self, index: str):
-        host_link = self.__get_es_link().replace('elastic', '127.0.0.1')
-        logger.warning(f"this scheme: {self.test_schemes, host_link}")
-        resp = requests.put("{}/{}".format(host_link, index), json=self.test_schemes)
-        if resp.status_code != 200:
-            logger.warning(f"Ошибка создания поискового индекса: {resp.status_code, index}")
-
-        else:
-            logger.warning(f"Create индекс: {resp.status_code, index}")
+        self.__get_es_client().indices.create(index=index_name, body=load_json(config_fname))
 
 
-if __name__ == '__main__':
-    settings = Settings()
-    print(settings.get_settings())
 
-    example =ESSaver()
-    print(example._ESSaver__get_es_link())
-    # print(example.test_get_scheme('genres'))
-    #1 create index
-    index = 'genres'
-    index_test = 'table4'
-    production_index = 'movies'
-    test_data = {
-    "text_field": "my pretty text test",
-    "number": 111
-    }
-
-    test_docs = [
-        {
-            "text_field": "my pretty text test2",
-            "number": 22
-        },
-        {
-            "text_field": "my pretty text test3",
-            "number": 33
-        }
-    ]
-
-    # print(example.test_create_index_127(index))
-    #2 delete index
-    # print(example.test_delete_index(index))
-
-    #3 create Elasticsearch connection
-    # print(example.test_get_connection())
-
-    #4 create test index(table)
-    # print(example.test_create_index_on_test_scheme(index_test))
-
-    #5 insert on record in index, created in point #4
-    # print(example.test_save_one(test_data, index_test))
-
-    #6 read index after insert data in point #
-    print(example.test_read_index(production_index))
-    # print(example.test_read_index('table3'))
-
-    #7 save_many for test index
-    # print(example.test_save_many(test_docs, index_test))
+    def create_index(self, client):
+        """Creates an index in Elasticsearch if one isn't already there."""
+        client.indices.create(
+            index="nyc-restaurants",
+            body={
+                "settings": {"number_of_shards": 1},
+                "mappings": {
+                    "properties": {
+                        "name": {"type": "text"},
+                        "borough": {"type": "keyword"},
+                        "cuisine": {"type": "keyword"},
+                        "grade": {"type": "keyword"},
+                        "location": {"type": "geo_point"},
+                    }
+                },
+            },
+            ignore=400,
+        )
 
 
+    def generate_actions(self):
+        """Reads the file through csv.DictReader() and for each row
+        yields a single document. This function is passed into the bulk()
+        helper to create many documents in sequence.
+        """
+        with open(DATASET_PATH, mode="r") as f:
+            reader = csv.DictReader(f)
+
+            for row in reader:
+                doc = {
+                    "_id": row["CAMIS"],
+                    "name": row["DBA"],
+                    "borough": row["BORO"],
+                    "cuisine": row["CUISINE DESCRIPTION"],
+                    "grade": row["GRADE"] or None,
+                }
+
+                lat = row["Latitude"]
+                lon = row["Longitude"]
+                if lat not in ("", "0") and lon not in ("", "0"):
+                    doc["location"] = {"lat": float(lat), "lon": float(lon)}
+                yield doc
+
+
+    def main(self):
+        print("Loading dataset...")
+        number_of_docs = download_dataset()
+
+        client = Elasticsearch('http://127.0.0.1:9200'
+            # Add your cluster configuration here!
+        )
+        print("Creating an index...")
+        create_index(client)
+
+        print("Indexing documents...")
+        progress = tqdm.tqdm(unit="docs", total=number_of_docs)
+        successes = 0
+        for ok, action in streaming_bulk(
+            client=client, index="nyc-restaurants", actions=generate_actions(),
+        ):
+            progress.update(1)
+            successes += ok
+        print("Indexed %d/%d documents" % (successes, number_of_docs))
+
+
+
+
+
+if __name__ == "__main__":
+    # es_link = "http://127.0.0.1:9200"
+    index = 'nyc-restaurants'
+    client = Elasticsearch('http://127.0.0.1:9200'
+                           # Add your cluster configuration here!
+                           )
+    # es = Elasticsearch(es_link)
+    # print(es)
+    # resp = requests.delete("{}/{}".format(es_link, index))
+    # if resp.status_code != 200:
+    #     logger.warning(f"Ошибка создания поискового индекса: {resp.status_code, index}")
+    # else:
+    #     logger.warning(f"DELETE индекс: {resp.status_code, index}")
+    example = ESSaver()
+    # print(example._ESSaver__get_es_link())
+    # print(example.create_index(client))
+    # print(example._ESSaver__delete_index(index))
+    print(example.get_schemes())
 
 
